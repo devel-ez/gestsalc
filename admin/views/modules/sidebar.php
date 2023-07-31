f8b9e4d7a3185fc19da31aab3bd704bf0c8a5be5
devel-ez <felipevelez.adm@hotmail.com>
2023-07-31 01:13:53 -0300

    Conteúdo principal em teste de rotas nos links do menu

--- admin/views/modules/sidebar.php
+++ admin/views/modules/sidebar.php
@@ -1,9 +1,9 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-warning elevation-4">
+<aside class="main-sidebar sidebar-light-warning elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-        <img src="views/assets/img/caveira.png" alt="Caveira Logo" style="width: 56px; height: 70px; margin-left: 05px">
-        <span class="brand-text font-weight-light" style="padding-left: 10px">GestSALC</span>
+    <a href="/" class="brand-link navbar-warning">
+        <img src="views/assets/img/caveira.png"  alt="Caveira Logo" style="width: 56px; height: 70px;">
+        <span class="brand-text font-weight-dark" style="padding-left: 25px; font-weight:bold; ">GestSALC</span>
     </a>
 
     <div class="sidebar">
@@ -18,35 +18,39 @@
         </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="/" class="nav-link active">
-                        <i class="nav-icon fas fa-home"></i>
+                    <a href="/" class="nav-link <?php if ($routesArray[1] == "") : ?>active<?php endif ?>">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Início
                         </p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="/dashboard" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <a href="processos" class="nav-link <?php if ($routesArray[1] == "processos") : ?>active<?php endif ?>">
+                        <i class="nav-icon fas fa-book"></i>
                         <p>
-                            Dashboard
-                            <i class="right fas fa-angle-left"></i>
+                            Processos
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
-                            </a>
-                        </li>
-                    </ul>
+                </li>
+                <li class="nav-item ">
+                    <a href="administradores" class="nav-link <?php if ($routesArray[1] == "administradores") : ?>active<?php endif ?>">
+                        <i class="fas fa-user-cog"></i>
+                        <p>Administradores</p>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="operadores" class="nav-link <?php if ($routesArray[1] == "operadores") : ?>active<?php endif ?>">
+                        <i class="fas fa-user-tie"></i>
+                        <p>Operadores</p>
+                    </a>
                 </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
     </div>
+</aside>
--- admin/views/template.php
+++ admin/views/template.php
@@ -1,10 +1,17 @@
+<?php
+
+$routesArray = explode("/", $_SERVER['REQUEST_URI']);
+$routesArray = array_filter($routesArray);
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_br">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GestSALC</title>
+    <link rel="icon" href="views/assets/img/martelo.png">
+
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -25,91 +32,42 @@
         ?>
         <!-- /.navbar -->
 
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-warning elevation-4">
-            <!-- Brand Logo -->
-            <a href="../../index3.html" class="brand-link">
-                <img src="views/assets/img/caveira.png" alt="Caveira Logo" style="width: 100px; height: 100px; margin-left: 10px">
-                <span class="brand-text font-weight-light" style="padding-left: 10px">GestSALC</span>
-            </a>
+        <!-- Sidebar -->
+        <?php
+        include "views/modules/sidebar.php";
+        ?>
+        <!-- /.sidebar -->
 
-            <!-- Sidebar -->
-            <?php
-            include "views/modules/sidebar.php";
-            ?>
-            <!-- /.sidebar -->
-        </aside>
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
+            <?php
+
+            echo '<pre>';
+            print_r($routesArray);
+            echo '</pre>';
+
+
+            if (!empty($routesArray[1])) {
+                if (
+                    $routesArray[1] == "processos" ||
+                    $routesArray[1] == "administradores" ||
+                    $routesArray[1] == "operadores"
+                ) {
+                    include "views/pages/" . $routesArray[1] . "/" . $routesArray[1] . ".php";
+                }
+            } else {
+                include "views/pages/inicio/inicio.php";
+            }
+            ?>
             <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>Fixed Layout</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item"><a href="#">Layout</a></li>
-                                <li class="breadcrumb-item active">Fixed Layout</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <!-- Default box -->
-                            <div class="card">
-                                <div class="card-header">
-                                    <h3 class="card-title">Title</h3>
-
-                                    <div class="card-tools">
-                                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                            <i class="fas fa-minus"></i>
-                                        </button>
-                                        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                                            <i class="fas fa-times"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    Start creating your amazing application!
-                                </div>
-                                <!-- /.card-body -->
-                                <div class="card-footer">
-                                    Footer
-                                </div>
-                                <!-- /.card-footer-->
-                            </div>
-                            <!-- /.card -->
-                        </div>
-                    </div>
-                </div>
-            </section>
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
 
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.2.0
-            </div>
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-        </footer>
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+        <!-- Footer -->
+        <?php include "views/modules/footer.php"; ?>
+        <!-- /.Footer -->
     </div>
     <!-- ./wrapper -->
 
